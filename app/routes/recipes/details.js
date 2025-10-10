4b704f0c2001c37f74e5b084e56534ce5e2161f4
Kimo <kimo.grafico@gmail.com>
2025-10-10 12:39:59 +0200

    Detalle, Create Recipe y Navbar

--- app/routes/recipes/details.js
+++ app/routes/recipes/details.js
@@ -3,8 +3,10 @@ import { inject as service } from '@ember/service';
 
 export default class RecipesDetailsRoute extends Route {
   @service store;
+  @service recipeData;
 
   async model(params) {
+    await this.recipeData.loadRecipes();
     return this.store.peekRecord('recipe', params.recipe_id);
   }
 }
---
+++ app/services/recipe-data.js
@@ -0,0 +1,49 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+
+export default class RecipeDataService extends Service {
+  @service store;
+
+  async loadRecipes() {
+    let response = await fetch('/api/recipes.json');
+    let data = await response.json();
+    let storedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+
+    const addRecipeToStore = (recipe) => {
+      let existingRecipe = this.store.peekRecord('recipe', recipe.id);
+
+      if (!existingRecipe) {
+        return this.store.createRecord('recipe', {
+          id: recipe.id,
+          title: recipe.title,
+          description: recipe.description,
+          ingredients: recipe.ingredients,
+          instructions: recipe.instructions,
+        });
+      }
+    };
+
+    storedRecipes.forEach(addRecipeToStore);
+    return data.recipes.map(addRecipeToStore);
+  }
+
+  async saveRecipe(recipe) {
+    let storedRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
+
+    storedRecipes.push(recipe);
+
+    localStorage.setItem('recipes', JSON.stringify(storedRecipes));
+  }
+
+  generateGUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      },
+    );
+  }
+
+}
